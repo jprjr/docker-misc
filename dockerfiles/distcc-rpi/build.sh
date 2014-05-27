27c16afd73d4988e2bbe5ad73233ffbb8897a31c
John Regan <john@jrjrtech.com>
2014-05-27 14:51:03 -0500

    Keeping the fs small while building

--- dockerfiles/distcc-rpi/build.sh
+++ dockerfiles/distcc-rpi/build.sh
@@ -20,10 +20,19 @@ makepkg --asroot -s -i --noconfirm
 cd / && rm -rf /tmp/packer
 
 # install the basic, non-conflicting stuff
+# docker build service has limited space,
+# need to clear out /tmp/packer* after each of these
 packer -S --noconfirm --noedit ${_target}-binutils 
+rm -rf /tmp/packer*
+
 packer -S --noconfirm --noedit ${_target}-linux-api-headers 
+rm -rf /tmp/packer*
+
 packer -S --noconfirm --noedit ${_target}-gcc-stage1
+rm -rf /tmp/packer*
+
 packer -S --noconfirm --noedit ${_target}-eglibc-headers 
+rm -rf /tmp/packer*
 
 # build gcc stage2 and remove gcc stage 1
 cd /tmp && packer -G ${_target}-gcc-stage2
@@ -31,6 +40,7 @@ cd ${_target}-gcc-stage2
 makepkg --asroot -s
 pacman -Rdd --noconfirm ${_target}-gcc-stage1
 pacman -U --noconfirm ${_target}-gcc-stage2*.pkg.tar*
+cd / && rm -rf /tmp/${_target}-gcc-stage2
 
 # build eglibc and remove eglibc-headers
 cd /tmp && packer -G ${_target}-eglibc
@@ -38,6 +48,7 @@ cd ${_target}-eglibc
 makepkg --asroot -s --noconfirm
 pacman -Rdd --noconfirm ${_target}-eglibc-headers
 pacman -U --noconfirm ${_target}-eglibc*.pkg.tar*
+cd / && rm -rf /tmp/${_target}-eglibc
 
 # build gcc and remove gcc-stage2
 cd /tmp && packer -G ${_target}-gcc
@@ -45,8 +56,7 @@ cd ${_target}-gcc
 makepkg --asroot 
 pacman -Rdd --noconfirm ${_target}-gcc-stage2
 pacman -U --noconfirm ${_target}-gcc*.pkg.tar*
-
-cd /
+cd / && rm -rf /tmp/${_target}-gcc
 
 # make handy symlinks for distcc
 mkdir /usr/bin/${_target}
